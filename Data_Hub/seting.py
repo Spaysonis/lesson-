82632e81da874f49411a74ee220ee1281b9f3c51
Spaysonis <spaysonis@gmail.com>
2025-01-28 11:51:29 +0200

    Update seting.py

--- Data_Hub/seting.py
+++ Data_Hub/seting.py
@@ -1,3 +1,3 @@
 API_URL_USER = 'https://gorest.co.in/public/v2/users'
 API_URL_POST = 'https://gorest.co.in/public/v2/posts'
-TOKEN = 'dae9a7ca700e6c3571e668daeeb0786fc7aeeded36389c10db94278cb99ea381'
+TOKEN = ''
